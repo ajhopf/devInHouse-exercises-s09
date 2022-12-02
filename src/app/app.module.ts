9f73d368f826c409cb08ff536ea1d198a1e1c378
André Hopf <andre.hopf@gmail.com>
2022-12-02 01:14:06 -0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SubjectsRegistryComponent } from './components/subjects-registry/subjects-registry.component';
-import { SendFormButtonComponent } from './components/send-form-button/send-form-button.component';
+import { SendFormButtonComponent } from './components/subjects-registry/send-form-button/send-form-button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SubjectsRegistryComponent,
-    SendFormButtonComponent
+    SendFormButtonComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule

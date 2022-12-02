e89f6cb42eeaf9b6fec2736444817fd9ea52b8ee
André Hopf <andre.hopf@gmail.com>
2022-12-02 00:32:13 -0300

    ex1 - add subject registry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubjectsRegistryComponent } from './components/subjects-registry/subjects-registry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectsRegistryComponent
   ],
   imports: [
     BrowserModule

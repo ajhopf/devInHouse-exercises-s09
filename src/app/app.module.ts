3cb05753af88f7bbd03b8fd8eb2818784f5e6a1c
André Hopf <andre.hopf@gmail.com>
2022-12-02 00:57:06 -0300

    ex2 - add modal de formulário cadastrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SubjectsRegistryComponent } from './components/subjects-registry/subjects-registry.component';
+import { SendFormButtonComponent } from './components/send-form-button/send-form-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubjectsRegistryComponent
+    SubjectsRegistryComponent,
+    SendFormButtonComponent
   ],
   imports: [
     BrowserModule
